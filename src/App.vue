ce31538179bc2e210f5eb68531190a9b560cc8a1
Kam <kamyshova.ea@yandex.com>
2020-12-24 20:20:45 +0300

    Half is done

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Container>
+            <ChatWindow>
+                <ChatMessage username="Ivan" time="21.12.2019 05:00:50">Hello, World!!!</ChatMessage>
+            </ChatWindow>
+        </Container>
+      </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Container from './components/Container.vue'
+import ChatWindow from './components/ChatWindow.vue'
+import ChatMessage from './components/ChatMessage.vue'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Container, ChatWindow, ChatMessage
+    }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+#body {
+  margin: 0;
+  background-color: #f9f9fa;
 }
 </style>
